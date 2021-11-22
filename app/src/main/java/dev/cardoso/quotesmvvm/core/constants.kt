e03c383dd09da9bc56a0616fd6cdc951e9357d39
lucila31 <lucila31>
2021-11-22 16:23:48 -0600

    Initial commit

---
+++ app/src/main/java/dev/cardoso/quotesmvvm/core/constants.kt
@@ -0,0 +1,7 @@
+package dev.cardoso.quotesmvvm.core
+
+
+//const val BASE_URL = "https://raw.githubusercontent.com/jomc-hash/mvc-usuarios/master/"
+const val BASE_URL = "http://192.168.42.29:8080"
+const val API_PATH="quotes.php"
+const val LOGIN_URL= "/api/v1/users/login"
